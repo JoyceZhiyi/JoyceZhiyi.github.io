2e17c0e576986490b9adad3ea304d44208885787
JoyceZhiyi <zhiyi_joyce@163.com>
2019-06-29 09:05:00 +0800

    index.html

--- Node.js/index.js
+++
@@ -1,5 +0,0 @@
-console.log("Hello World!!");
-
-// node.js  可以把文件模块化，将文件引进来。
-// module.exports = 将函数输出
-// require 
--- js/windows.js
+++
@@ -1,9 +0,0 @@
-function SubMenuout(li){
-    var subMenu = li.getElementsByTagName("ul")[0];
-    subMenu.style.display = "block";
-}
-
-function SubMenunone(li){
-    var subMenu = li.getElementsByTagName("ul")[0];
-    subMenu.style.display = "none";
-}
